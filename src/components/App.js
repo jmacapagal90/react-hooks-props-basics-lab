b6f23d61c6298dea9db9e92ec84b0c81ab2ef1d1
James Macapagal <j.macapagal@gmail.com>
2022-06-07 20:26:25 -0500

    commit tree 85d0fd1c25e6bbbbc3965d26628c9890c73fee73

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
+  const {name,city,bio,color,links} = user
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} color={color} />
+      <About bio={bio} links={links}/>
     </div>
   );
 }
